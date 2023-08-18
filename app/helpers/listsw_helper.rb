2590fa24c889b5cecd177ffc2c6f0e537848662a
arthur rohr <arthur.rohr.pcc@gmail.com>
2023-08-18 11:06:57 +0200

    Controllers, models, views, and some Styling

---
+++ app/helpers/listsw_helper.rb
@@ -0,0 +1,2 @@
+module ListswHelper
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+require 'open-uri'
+
+Movie.destroy_all
+
+puts "Generating movies..."
+
+20.times do
+  title = Faker::Movie.title
+  overview = Faker::Movie.quote
+  random_image_number = rand(1..2000)
+  poster_url = "https://source.unsplash.com/featured/?fantasy&#{random_image_number}"
+  rating = rand(1.0..10.0).round(1)
+
+  Movie.create(
+    title: title,
+    overview: overview,
+    poster_url: poster_url,
+    rating: rating
+  )
+end
+
+puts "20 Movie added"
